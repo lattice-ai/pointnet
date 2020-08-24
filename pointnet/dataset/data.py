17d88fe3a932ea63ff004b870ddf8e4f9ddf0bf6
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-24 20:40:16 +0530

    Added Tensorflow Dataset and Mesh Visualization Utils

---
+++ pointnet/dataset/data.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+
+
+def read_labeled_tfrecord(example):
+    example = tf.io.parse_single_example(
+        example, {
+            'x': tf.io.FixedLenFeature([2048], tf.float32),
+            'y': tf.io.FixedLenFeature([2048], tf.float32),
+            'z': tf.io.FixedLenFeature([2048], tf.float32),
+            'label': tf.io.FixedLenFeature([], tf.int64),
+        }
+    )
+    mesh = tf.stack(
+        [example['x'], example['y'], example['z']], axis=-1)
+    label = example['label']
+    return mesh, label
+
+
+def get_dataset(tfrecord_files):
+    ignore_order = tf.data.Options()
+    ignore_order.experimental_deterministic = False
+    dataset = tf.data.TFRecordDataset(
+        tfrecord_files,
+        num_parallel_reads=tf.data.experimental.AUTOTUNE
+    ).with_options(ignore_order).map(read_labeled_tfrecord)
+    dataset = dataset.repeat()
+    dataset = dataset.shuffle(2048)
+    dataset = dataset.batch(32)
+    dataset = dataset.prefetch(
+        tf.data.experimental.AUTOTUNE
+    )
+    return dataset
---
+++ pointnet/utils.py
@@ -0,0 +1,15 @@
+import plotly.graph_objects as go
+
+
+def get_visualization_figure(mesh, label):
+    x, y, z = mesh.numpy()[0].T
+    fig = go.Figure(
+        data=[
+            go.Scatter3d(
+                x=x, y=y, z=z,
+                mode='markers'
+            )
+        ]
+    )
+    fig.update_layout(title=label)
+    return fig
