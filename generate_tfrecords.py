b195a7c26878a785703c685044bf8a19637a24a8
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-24 20:10:05 +0530

    Added driver code to generate_tfrecords

--- generate_tfrecords.py
+++ generate_tfrecords.py
@@ -6,9 +6,13 @@ from pointnet.dataset.download import download_dataset
 def generate_tfrecords():
     data_path = download_dataset(
         "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip",
-        "modelnet10.zip"
+        "modelnet10.zip", "ModelNet10"
     )
     recorder = TFRecordCreator(data_path, 1000)
     try: os.mkdir('./tfrecords')
     except: pass
     recorder.create_records('./tfrecords')
+
+
+if __name__ == '__main__':
+    generate_tfrecords()
