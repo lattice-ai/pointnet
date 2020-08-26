3fa856e7594889b5b0acf1cceb1f3fe5bd46efa6
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-27 00:17:18 +0530

    Updated Jitter and added Rotation

--- pointnet/dataset/data.py
+++ pointnet/dataset/data.py
@@ -1,5 +1,7 @@
+import math
 import tensorflow as tf
 from tensorflow.python import data
+from tensorflow.python.keras.backend import dtype
 
 
 def read_labeled_tfrecord(example):
@@ -17,11 +19,25 @@ def read_labeled_tfrecord(example):
     return mesh, label
 
 
-def apply_augmentation(mesh, label):
-    mesh += tf.random.uniform(mesh.shape, -0.005, 0.005, dtype=tf.float32)
-    # # shuffle points
-    # mesh = tf.random.shuffle(mesh)
+def apply_jitter(mesh, label):
+    mesh += tf.random.normal(
+        mesh.shape, mean=0.0, stddev=0.02,
+        dtype=tf.float32, seed=69
+    )
     return mesh, label
+
+
+def apply_rotation(mesh, label):
+    # rotated_data = tf.zeros(mesh.shape, dtype=tf.float32)
+    theta = tf.random.uniform([]) * 2 * tf.constant(math.pi)
+    sin_theta = tf.math.sin(theta)
+    cos_theta = tf.math.cos(theta)
+    rotation_matrix = tf.convert_to_tensor(
+        [[cos_theta, 0, sin_theta],
+        [0, 1, 0], [-sin_theta, 0, cos_theta]]
+    )
+    rotated_mesh = tf.tensordot(mesh, rotation_matrix)
+    return rotated_mesh, label
 
 
 def get_dataset(tfrecord_files, buffer_size, batch_size, augment):
@@ -31,7 +47,9 @@ def get_dataset(tfrecord_files, buffer_size, batch_size, augment):
         tfrecord_files,
         num_parallel_reads=tf.data.experimental.AUTOTUNE
     ).with_options(ignore_order).map(read_labeled_tfrecord)
-    if augment: dataset = dataset.map(apply_augmentation)
+    if augment:
+        dataset = dataset.map(apply_rotation)
+        dataset = dataset.map(apply_jitter)
     dataset = dataset.shuffle(buffer_size)
     dataset = dataset.batch(batch_size)
     dataset = dataset.prefetch(
