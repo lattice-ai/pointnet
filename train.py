ff87fb9c4bc8d5c404dc743ad5d0e5cf2fc350d9
soumik12345 <19soumik.rakshit96@gmail.com>
2020-08-29 02:40:12 +0530

    Added polynomial lr decay

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from glob import glob
-from pointnet.trainer import Trainer
+from pointnet.trainer import Trainer, tf
 
 
 def run_training():
@@ -9,7 +9,11 @@ def run_training():
         'train_tfrecord_files': glob('./tfrecords/train*'), 'train_files': 4000,
         'test_tfrecord_files': glob('./tfrecords/test*'), 'test_files': 908,
         'buffer_size': 2048, 'batch_size': 32, 'num_points': 2048, 'n_classes': 10,
-        'weight_file': 'classification-modelnet10', 'epochs': 50, 'learning-rate': 0.001
+        'weight_file': 'classification-modelnet10', 'epochs': 50,
+        'learning-rate': tf.keras.optimizers.schedules.PolynomialDecay(
+            0.001, 10000, end_learning_rate=0.0001,
+            power=0.9, cycle=False, name=None
+        )
     }
 
     trainer = Trainer(configs)
